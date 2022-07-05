f1f164499bb62f3d700a6fc15db075947ab4d684
Oussama Sethoum <os.sethoum@gmail.com>
2022-07-05 06:11:00 +0100

    add graphql config

---
+++ src/ui/pages/home.tsx
@@ -0,0 +1,9 @@
+import { Title, Center } from "@mantine/core";
+
+export default function Home() {
+  return (
+    <Center sx={{ height: "100vh" }}>
+      <Title>Home</Title>
+    </Center>
+  );
+}
