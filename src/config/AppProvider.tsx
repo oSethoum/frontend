1a3e06e465b0979ca126e41f92a68120157a4bb4
Oussama Sethoum <os.sethoum@gmail.com>
2022-07-05 06:25:30 +0100

    add ltr support

--- src/config/AppProvider.tsx
+++ src/config/AppProvider.tsx
@@ -1,15 +1,23 @@
-import { MantineProvider } from "@mantine/core";
+import { createEmotionCache, MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { NotificationsProvider } from "@mantine/notifications";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { createClient, Provider } from "urql";
 import { useTokenStore } from "./auth";
+import rtlPlugin from "stylis-plugin-rtl";
 
 interface IAppProvider {
   children: React.ReactNode;
 }
 
 export function AppProvider({ children }: IAppProvider) {
+  // load config and theme always before rendering the app
+  const [rtl, setRtl] = useState(false);
+  const rtlCache = createEmotionCache({
+    key: "mantine-rtl",
+    stylisPlugins: [rtlPlugin],
+  });
+
   const { token } = useTokenStore();
   const client = useMemo(
     () =>
@@ -28,9 +36,10 @@ export function AppProvider({ children }: IAppProvider) {
   return (
     <Provider value={client}>
       <MantineProvider
-        theme={{ colorScheme: "dark" }}
+        theme={{ colorScheme: "dark", dir: rtl ? "rtl" : "ltr" }}
         withGlobalStyles
         withNormalizeCSS
+        emotionCache={rtl ? rtlCache : undefined}
       >
         <NotificationsProvider>
           <ModalsProvider>{children}</ModalsProvider>
