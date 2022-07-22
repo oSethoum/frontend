056649ca429498b9d19c42c467c02c58fbc3584b
oSethoum <os.sethoum@gmail.com>
2022-07-22 18:01:14 +0100

    remove zstund

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "@/ui";
-import { Box } from "@mantine/core";
 import { createClient, Provider } from "urql";
-import {
-  createContext,
-  Dispatch,
-  SetStateAction,
-  useMemo,
-  useState,
-} from "react";
+import { createContext, useMemo, useState } from "react";
 import { useLocalStorage } from "@mantine/hooks";
-
-interface User {
-  id: number;
-  username: string;
-}
 
 interface IAppContext {
   url: string;
   token: string;
-  user: User | null;
-  setUser: Dispatch<SetStateAction<User | null>>;
   setToken: (val: string | ((prevState: string) => string)) => void;
 }
 
 export const AppContext = createContext<IAppContext>({
   url: "",
   token: "",
-  user: null,
-  setUser: () => {},
   setToken: () => {},
 });
 
 function App() {
-  const [user, setUser] = useState<User | null>(null);
+  const [url, setUrl] = useState("127.0.0.1:3001");
   const [token, setToken] = useLocalStorage({
     key: "jwtToken",
     defaultValue: "",
   });
-  const [url, setUrl] = useState("http://localhost:3001");
   const client = useMemo(
     () =>
       createClient({
@@ -54,23 +37,13 @@ function App() {
   );
 
   return (
-    <AppContext.Provider value={{ url, token, setToken, user, setUser }}>
+    <AppContext.Provider value={{ url, token, setToken }}>
       <Provider value={client}>
-        <Box
-          sx={(theme) => ({
-            height: "100vh",
-            backgroundColor:
-              theme.colorScheme === "dark"
-                ? theme.colors.dark[6]
-                : theme.colors.gray[1],
-          })}
-        >
-          <Router>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-            </Routes>
-          </Router>
-        </Box>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </Router>
       </Provider>
     </AppContext.Provider>
   );
